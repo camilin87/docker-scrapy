1cdcb9f610d74f4d8a12d61d6d3c3d597b654a4b
camilin87 <camilin87@gmail.com>
2017-01-12 20:58:13 -0500

    build an image

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,34 @@
-task :build, [:image_name] do |t, args|
+task :build, [:image_name] => [:clean] do |t, args|
     image_name = args[:image_name]
     raise "image_name required" unless image_name
+    full_image_name = "camilin87/#{image_name}"
 
-    puts "Valid image_name received"
+    puts "Generating #{full_image_name}..."
 
-    # pwd = File.dirname(__FILE__)
+    pwd = File.dirname(__FILE__)
 
-    # requirements_file = File.join(pwd, "requirements/", image_name, )
+    requirements_file = File.join(pwd, "requirements/", image_name, "./requirements.txt")
+
+    raise "Requirements file #{requirements_file} not found" unless File.exist?(requirements_file)
+
+    mkdir "gen"
+
+    tmp_requirements_file = File.join(pwd, "./gen/requirements.txt")
+    cp requirements_file, tmp_requirements_file
+
+    dockerfile = File.join(pwd, "./Dockerfile")
+    tmp_dockerfile = File.join(pwd, "./gen/Dockerfile")
+    cp dockerfile, tmp_dockerfile
+
+    Dir.chdir("gen") do
+        sh %{
+            docker build -f Dockerfile -t #{full_image_name} .
+        }
+    end
+
+    puts "Successfully generated #{full_image_name}"
+end
+
+task :clean do
+    rm_rf "gen"
 end
